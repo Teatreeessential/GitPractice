20eca64c835f7786521d13d4aa7e026b6b0aeeb2
An_Teatreeessential <wdefrg1324@gmail.com>
2019-08-11 22:12:34 +0900

    count 추가

--- test1.js
+++ test1.js
@@ -1,10 +1,15 @@
 function Vehicle (name, speed) {
+    if (!Vehicle.count) Vehicle.count = 0; // 함수 메모이제이션 초기화
+    Vehicle.count++; // 전세계에 존재하는 탈것은 몇대인가??
     this.name = name;
     this.speed = speed;
 }
 
 Vehicle.prototype.drive = function (){
     console.log(this.name + '가' + this.speed + '로 달립니다.');
+}
+Vehicle.prototype.getCount = function () {
+    console.log(Vehicle.count);
 }
 
 
@@ -31,6 +36,7 @@ Truck.prototype.load = function () {
 }
 
 let sedan = new Sedan('제네시스', 100, 180);
+let sedan2 = new Sedan('에쿠스', 200, 300);
 let truck = new Truck('포터', 80, '컴퓨터');
 
 sedan.drive();
@@ -38,5 +44,8 @@ sedan.boost();
 
 truck.drive();
 truck.load();
+
+sedan.getCount();
+truck.getCount();
 
 
